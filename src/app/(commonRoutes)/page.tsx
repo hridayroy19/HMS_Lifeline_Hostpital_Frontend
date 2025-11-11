b80a7135c5ae52a2f80fac9849bae57af8aa45fa
hridayroy19 <hrhridoyroy503@gmail.com>
2025-11-11 19:25:23 +0600

    feat:(banner)create banner section

--- src/app/(commonRoutes)/page.tsx
+++ src/app/(commonRoutes)/page.tsx
@@ -1,12 +1,12 @@
+import Banner from "@/containers/home_page/banner/banner";
 import React from "react";
 
 const HomePage = () => {
   return (
-    <div className="py-32">
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus non,
-      iste inventore, minima rem deleniti laboriosam unde exercitationem magnam
-      saepe aut fuga numquam voluptates voluptate perspiciatis repudiandae.
-      Assumenda, sint tempora?
+    <div className="section-padding-y">
+      <section>
+        <Banner />
+      </section>
     </div>
   );
 };
