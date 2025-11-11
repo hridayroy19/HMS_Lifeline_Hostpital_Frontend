8aab112eac83d7909524e99757179ba45adbc795
hridayroy19 <hrhridoyroy503@gmail.com>
2025-11-11 19:49:32 +0600

    feat:(why chose) crate why chose section and responsive

--- src/app/(commonRoutes)/page.tsx
+++ src/app/(commonRoutes)/page.tsx
@@ -1,4 +1,5 @@
 import Banner from "@/containers/home_page/banner/banner";
+import WhyChooseUsSection from "@/containers/home_page/whyChooseUs/whyChooseUs";
 import React from "react";
 
 const HomePage = () => {
@@ -6,6 +7,9 @@ const HomePage = () => {
     <div className="section-padding-y">
       <section>
         <Banner />
+      </section>
+      <section>
+      <WhyChooseUsSection/>
       </section>
     </div>
   );
